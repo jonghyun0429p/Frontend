177c6fc38d4fea44f5ad41fb11e19d296a40f19a
SeokjinDev <tjrwls1108011@gmail.com>
2024-06-12 04:32:52 +0900

    Fix : Import Component Import Link Component

--- frontend/src/layout/Header1.jsx
+++ frontend/src/layout/Header1.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from '../styles/Header1.module.css';
+import { Link } from 'react-router-dom';
 
 const Header1 = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>로고위치</div>
+      <Link to='/MainPage'>
+        <div className={styles.logo}>로고위치</div>
+      </Link>
       <div className={styles.headerButtons}>
         <button className={styles.headerButton}>로그인</button>
         <button className={styles.headerButton}>회원가입</button>
